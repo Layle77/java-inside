db1280c59bee32b921fbdacd001de7303e7a4252
NIVOIX JULIEN <jnivoix@localhost.localdomain>
2018-10-16 10:25:35 +0200

    update lololo

--- lab4/src/main/java/fr.umlv.labFour/fr/umlv/labFour/Exemple1.java
+++ lab4/src/main/java/fr.umlv.labFour/fr/umlv/labFour/Exemple1.java
@@ -6,9 +6,11 @@ public class Exemple1 {
 		// TODO Auto-generated method stub
 		var continuationScope = new ContinuationScope("hello1");
 	    Continuation suite = new Continuation(continuationScope, ()->{
-//	    	Continuation.yield(continuationScope);
+	    	Continuation.yield(continuationScope);
+	    	System.out.println(Continuation.getCurrentContinuation(continuationScope));
 	    	System.out.println("hello Continuation");
 	    	});
+	    System.out.println(Continuation.getCurrentContinuation(continuationScope));
 	    suite.run();
 	    suite.run();
 	}
